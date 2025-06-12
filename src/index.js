98a830ee08528336fd8878489f6c4791518a7f59
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2025-06-12 16:16:34 +0300

    new-data-brevo

--- src/index.js
+++ src/index.js
@@ -11,13 +11,3 @@ const bootstrap = async () => {
 };
 
 void bootstrap();
-
-// import { initMongoConnection } from './db/initMongoConnection.js';
-// import { setupServer } from './server.js';
-
-// const bootstrap = async () => {
-//   await initMongoConnection();
-//   setupServer();
-// };
-
-// bootstrap();
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -52,19 +52,3 @@ export const updateContactSchema = Joi.object({
     'any.only': 'Contact type must be one of: work, home, personal',
   }),
 });
-
-// export const createContactSchema = Joi.object({
-//   name: Joi.string().min(3).max(20).required(),
-//   phoneNumber: Joi.string().min(3).max(20).required(),
-//   email: Joi.string().email(),
-//   isFavourite: Joi.boolean(),
-//   contactType: Joi.string().valid('work', 'home', 'personal').required(),
-// });
-
-// export const updateContactSchema = Joi.object({
-//   name: Joi.string().min(3).max(20),
-//   phoneNumber: Joi.string().min(3).max(20).required(),
-//   email: Joi.string().email(),
-//   isFavourite: Joi.boolean(),
-//   contactType: Joi.string().valid('work', 'home', 'personal'),
-// });
